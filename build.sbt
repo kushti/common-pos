db55529d951ebcac9f905e6779a5e16be7b8c534
kushti <kushtech@yahoo.com>
2015-11-18 12:38:03 +0300

    build.sbt / Simulator stub

---
+++ build.sbt
@@ -0,0 +1,10 @@
+name := "common-pos"
+
+version := "1.0"
+
+scalaVersion := "2.11.7"
+
+libraryDependencies ++= Seq(
+  "com.typesafe.akka" %% "akka-actor" % "2.4.0"
+)
+    
---
+++ src/main/scala/scorex/simulation/Simulator.scala
@@ -0,0 +1,14 @@
+package scorex.simulation
+
+import akka.actor.ActorSystem
+
+object Simulator {
+
+  def main(args: Array[String]): Unit = {
+
+    val system = ActorSystem()
+
+
+
+  }
+}
